b12d1123767cdb9463ab9ba2fd9d616b12e5075f
Michael Head <michaelehead@gmail.com>
2022-08-21 21:26:45 -0400

    change src location based on app location

--- src/text-tracker-loader.js
+++ src/text-tracker-loader.js
@@ -4,9 +4,15 @@
     return null;
   }
 
+  let src = "https://text-tracker-fcbec.web.app/text-tracker-es.js";
+
+  if (window.location.hostname === "localhost") {
+    src = "../dist/text-tracker-es.js";
+  }
+
   const script = document.createElement("script");
   script.type = "module";
-  script.src = "../dist/text-tracker-es.js";
+  script.src = src;
 
   script.onload = () => {
     const el = document.createElement('text-tracker');
